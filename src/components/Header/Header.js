8a8dc57a014ffc7bb15f109f862909c68fdbf194
Youngbeom Choi <helloybz@gmail.com>
2021-11-10 23:36:43 +0900

    Init and make header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+export function Header() {
+    return (
+        <Box >
+            <AppBar sx={{ bgcolor: 'rgb(13, 25, 40)', borderBottom: 'solid 1px rgb(24, 47, 75)' }}>
+                <Toolbar>
+                    <Typography sx={{ fontSize: 'h5.fontSize', fontWeight: '600', color: 'rgb(243, 246, 249)' }}>
+                        helloybz.
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
