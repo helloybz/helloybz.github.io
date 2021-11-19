114e0186d298c288fce8cc03c36b588bd0e372ab
Youngbeom Choi <helloybz@gmail.com>
2021-11-19 11:56:17 +0900

    Display only deepwalk

--- src/api.js
+++ src/api.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
 
+const target_reposotory = [
+    // 'CLANE',
+    'deepwalk-clone',
+    // 'demucs-clone',
+    // 'pggan-clone',
+    // 'npg',
+    // 'AmSanWang',
+]
+
+
 export function getRepositories() {
     return axios.get(
         'https://asia-northeast3-github-api-332405.cloudfunctions.net/get_github_repos',
     ).then((response) => {
         var new_data = []
         for (let i = 0; i < response.data.length; i++) {
-            if (['CLANE', 'deepwalk-clone', 'demucs-clone', 'pggan-clone', 'npg', 'AmSanWang'].includes(response.data[i].name)) {
+            if (target_reposotory.includes(response.data[i].name)) {
                 new_data.push({ ...response.data[i] });
             }
         }
